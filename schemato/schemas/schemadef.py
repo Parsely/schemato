49d111218eb74b14dc7114e7a2eb8a3d74dbfbbf
emmett9001 <ebutler9@gmail.com>
2012-09-25 12:31:51 -0400

    continue refactoring schemato

--- schemato/schemas/schemadef.py
+++ schemato/schemas/schemadef.py
@@ -1,19 +1,39 @@
+import rdflib.term as rt
+from pyMicrodata import pyMicrodata
+from pyRdfa import pyRdfa
+import rdflib
+
+from collections import defaultdict
+
+
 # what functionality is common to every single schema def?
 class SchemaDef(object):
+    """class to handle the loading and caching of a standard"""
     def __init__(self):
         super(SchemaDef, self).__init__()
         self.ontology_file = ""
-        self.ontology_graph = None
+        self.ontology = defaultdict(list)
+        self.attributes_by_class = defaultdict(list)
         self.ontology_parser_function = None
 
     def parse_ontology(self):
-        """ABSTRACT - parse the ontology file into a graph"""
+        """parse the ontology file into a graph"""
+        # problem: it makes sense to have this implementation be common
+        # between the below two subclasses (ie have it implemented here)
+        # however, it depends on ontology_parser_function being set,
+        # which happens in the subclasses. maybe no big deal, but worth being
+        # thoughtful about
         raise NotImplementedError
 
 # what separate functionality does an RdfSchemaDef have
 # from a MicrodataSchemaDef?
 class RdfSchemaDef(SchemaDef):
-    pass
+    def __init__(self):
+        super(RdfSchemaDef, self).__init__()
+        self.ontology_parser_function = lambda s: rdflib.Graph().parse(s, format='n3')
+
 
 class MicrodataSchemaDef(SchemaDef):
-    pass
+    def __init__(self):
+        super(MicrodataSchemaDef, self).__init__()
+        self.ontology_parser_function = lambda s: pyRdfa().graph_from_source(s)

7a32b946ea43020d2ad012507544788f24a7bcbf
emmett9001 <emmett.butler321@gmail.com>
2012-09-26 13:08:42 -0400

    separate out ugly conditional with subclasses

--- schemato/schemas/opengraph.py
+++ schemato/schemas/opengraph.py
@@ -2,7 +2,15 @@ from validator import RdfValidator
 from schemadef import RdfSchemaDef
 
 class OpenGraphSchemaDef(RdfSchemaDef):
-    pass
+    def parse_ontology(self):
+        for subj, pred, obj in self._schema_nodes()
+            leaves = [(subj, pred, obj)]
+            if type(obj) == rt.BNode:
+                leaves = deepest_node((subj, pred, obj), graph)
+
+            for s,p,o in leaves:
+                if s not in self.attribs_by_class[self.source] and not isinstance(s, rt.BNode):
+                    self.attribs_by_class[self.source].append(s)
 
 class OpenGraphValidator(RdfValidator):
     pass

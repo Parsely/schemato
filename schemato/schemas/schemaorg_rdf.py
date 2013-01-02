832fd2c2c0ffe80e3f79a6a4e54cd88797d69674
emmett9001 <emmett.butler321@gmail.com>
2013-01-02 13:27:26 -0500

    add a validator for the rdfa implementation of schema.org this implementation is used on semanticweb.com alongside rnews, which causes some problems

---
+++ schemato/schemas/schemaorg_rdf.py
@@ -0,0 +1,14 @@
+from validator import RdfValidator
+from schemadef import RdfSchemaDef
+
+class SchemaOrgRDFaSchemaDef(RdfSchemaDef):
+    def __init__(self):
+        super(SchemaOrgRDFaSchemaDef, self).__init__()
+        self._ontology_file = "http://schema.org/docs/schema_org_rdfa.html"
+        self.parse_ontology()
+
+class SchemaOrgRDFaValidator(RdfValidator):
+    def __init__(self, graph, doc_lines, url=""):
+        super(SchemaOrgRDFaValidator, self).__init__(graph, doc_lines, url=url)
+        self.schema_def = SchemaOrgRDFaSchemaDef()
+        self.allowed_namespaces = ["http://schema.org/"]
--- schemato/settings.py
+++ schemato/settings.py
@@ -1,5 +1,6 @@
 VALIDATOR_MODULES = [
     "schemas.rnews.RNewsValidator",
     "schemas.opengraph.OpenGraphValidator",
-    #"schemas.schemaorg.SchemaOrgValidator"
+    "schemas.schemaorg.SchemaOrgValidator",
+    "schemas.schemaorg_rdf.SchemaOrgRDFaValidator"
 ]

9fbc4894d525ab3d9a6342327b68b09e0ec4a694
Emmett Butler <emmett.butler321@gmail.com>
2014-05-06 15:37:07 -0400

    fix pypi installation crash

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from distutils.core import setup
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
 reqs = ['rdflib',
         'lepl',
         'html5lib',
@@ -11,14 +8,13 @@ reqs = ['rdflib',
 
 setup(
     name="schemato",
-    version="1.1",
+    version="1.1.1",
     author='Emmett Butler',
     author_email='emmett@parsely.com',
     url='https://github.com/Parsely/schemato',
     keywords='microdata rdf metadata',
     packages=['schemato'],
     description='Unified semantic metadata validator',
-    long_description=long_description,
     platforms='any',
     classifiers=[
         'Development Status :: 3 - Alpha',

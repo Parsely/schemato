edab53c539548bc8ef6aa12b454bb36e01352c10
Emmett Butler <emmett.butler321@gmail.com>
2014-05-19 16:10:34 -0400

    install all necessary packages with setup.py remove unused pip dependencies

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup
 
 reqs = ['rdflib',
         'lepl',
-        'html5lib',
-        'lxml',
+        'lxml'
         ]
 
 setup(
@@ -13,7 +12,7 @@ setup(
     author_email='emmett@parsely.com',
     url='https://github.com/Parsely/schemato',
     keywords='microdata rdf metadata',
-    packages=['schemato'],
+    packages=['schemato', 'schemato.schemas'],
     description='Unified semantic metadata validator',
     platforms='any',
     classifiers=[
